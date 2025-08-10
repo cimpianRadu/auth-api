acdc7e80d504fc7c7392bd3f73fd1dd45476ceb7
cimpianRadu <cimpian.radu@yahoo.com>
2025-08-10 14:19:08 +0300

    Improve create user

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -9,10 +9,27 @@ router.get("/public", (req, res) => {
 
 router.post("/create-user", async (req, res) => {
   const { clerkId, email } = req.body;
-  const user = await prisma.user.create({
-    data: { clerkId, email },
-  });
-  res.json({ user });
+  try {
+    if (!clerkId || !email) {
+      return res.status(400).json({ message: "Clerk ID and email are required" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { clerkId },
+    });
+
+    if (user) {
+      return res.status(200).json({ message: "User already exists" });
+    }
+
+    const newUser = await prisma.user.create({
+      data: { clerkId, email },
+    });
+
+    return res.status(201).json({ message: "User created successfully", user: newUser.email });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 export default router;
