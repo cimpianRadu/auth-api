2f1f305492f5fe2684162dbed2721ee9fd67232c
cimpianRadu <cimpian.radu@yahoo.com>
2025-08-10 14:52:01 +0300

    Add some logs for create user

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -9,6 +9,7 @@ router.get("/public", (req, res) => {
 
 router.post("/create-user", async (req, res) => {
   const { clerkId, email } = req.body;
+  console.log("✅✅✅ clerkId, email", clerkId, email);
   try {
     if (!clerkId || !email) {
       return res.status(400).json({ message: "Clerk ID and email are required" });
@@ -19,13 +20,14 @@ router.post("/create-user", async (req, res) => {
     });
 
     if (user) {
+      console.log("🔴🔴🔴 User already exists", user);
       return res.status(200).json({ message: "User already exists" });
     }
 
     const newUser = await prisma.user.create({
       data: { clerkId, email },
     });
-
+    console.log("🟢🟢🟢 User created successfully", newUser);
     return res.status(201).json({ message: "User created successfully", user: newUser.email });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
