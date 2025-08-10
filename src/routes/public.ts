123eeeb990c9ac2b4092ece836d806deac205e20
cimpianRadu <cimpian.radu@yahoo.com>
2025-08-10 14:08:21 +0300

    add create user route

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { prisma } from "../db";
 
 const router = Router();
 
 router.get("/public", (req, res) => {
   res.json({ message: "This is a public endpoint — no auth needed." });
+});
+
+router.post("/create-user", async (req, res) => {
+  const { clerkId, email } = req.body;
+  const user = await prisma.user.create({
+    data: { clerkId, email },
+  });
+  res.json({ user });
 });
 
 export default router;
